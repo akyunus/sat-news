3c8d1db6b8838be4dcd1ad50a4a71737a4d2bb3b
Yunus Ak <yunus.ak.gen.tr@gmail.com>
2017-09-21 00:23:20 +0300

    index.php güncelleme

--- public_html/index.php
+++ public_html/index.php
@@ -1,25 +1,20 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+// birkaç farklı yerde kullanılacak olan APP_DIR değişkeni.
+// 
+define('APP_DIR',dirname(__DIR__).DIRECTORY_SEPARATOR.'sat-news-app'.DIRECTORY_SEPARATOR);
+
+// composer ile beraber gelen autoloader sistemini tanıtılması
+require_once APP_DIR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
+
 
 $app = new Silex\Application();
 
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/../twig-dosyalari',
-));
+// ön tanımlamalar
+require_once APP_DIR.'registers.php';
 
-$app->get('/', function () use ($app) {
-		
-	return $app['twig']->render('minimal-content.html', array());
-
-});
-
-$app->get('/test/', function () {
-	
-	return "test sayfası";
-
-});
-
+// rota tanımları
+require_once APP_DIR.'routes.php';
 
 
 $app->run();
